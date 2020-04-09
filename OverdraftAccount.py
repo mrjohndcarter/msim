d8c9aa9270e2d2f066725d75d8f46e3c13f9bda9
John Carter <mrjohndcarter@gmail.com>
2020-04-09 15:24:54 -0400

    Withdrawal for accounts with overdrafts.
    Bug: second w/d while in overdraft will fail.

--- OverdraftAccount.py
+++ OverdraftAccount.py
@@ -7,10 +7,23 @@ class OverdraftAccount(Account):
         super().__init__(name, number)
         self.overdraft_account = Account(name, number)
         self.overdraft_account.balance = overdraft
+        self.overdraft_maximum = overdraft
 
     def get_overdraft_amount(self):
         return self.overdraft_account.balance
 
+    def transact(self, amount: float) -> float:
+# withdraw -> take from balance first, then O/D
+# deposit -> put toward O/D first then balance
+        # withdrawal logic:
+        if amount < 0:
+            self.balance += amount
+
+            # if account has gone into negative
+            if self.balance < 0:
+                self.overdraft_account.transact(self.balance)
+
+        return self.balance
 
 class TestOverdraftAccount(TestCase):
     def setUp(self) -> None:
@@ -22,3 +35,12 @@ class TestOverdraftAccount(TestCase):
         self.assertEqual(self.a.get_overdraft_amount(), 500)
         self.assertEqual(self.b.get_overdraft_amount(), 750)
         self.assertEqual(self.c.get_overdraft_amount(), 0)
+
+    def test_overdraft_maximums(self):
+        self.assertEqual(self.a.overdraft_maximum, 500)
+        self.assertEqual(self.b.overdraft_maximum, 750)
+        self.assertEqual(self.c.overdraft_maximum, 0)
+
+    def test_withdraw_with_overdraft(self):
+        self.a.transact(-300)
+        self.assertEqual(self.a.balance, -300)
