7b96d780483ccd5163e5ff41e24846126948c388
John Carter <mrjohndcarter@gmail.com>
2020-04-09 16:08:02 -0400

    Fixed my expected, actual ordering.

--- Account.py
+++ Account.py
@@ -29,24 +29,24 @@ class TestAccount(TestCase):
         self.b.balance = 1000.0
 
     def test_new_account(self):
-        self.assertEqual(self.a.balance, 0)
+        self.assertEqual(0, self.a.balance)
 
     def test_deposit(self):
         self.a.transact(505.50)
-        self.assertEqual(self.a.balance, 505.50)
-        self.assertEqual(self.a.transact(10), 515.50)
+        self.assertEqual(505.50, self.a.balance)
+        self.assertEqual(515.50, self.a.transact(10))
 
     def test_withdrawal_no_overdraft(self):
         self.b.transact(-500)
-        self.assertEqual(self.b.balance, 500)
-        self.assertEqual(self.b.transact(-100), 400)
+        self.assertEqual(500, self.b.balance)
+        self.assertEqual(400, self.b.transact(-100))
 
     def test_withdrawal_no_overdraft_attempted_overdraw(self):
         with self.assertRaises(InsufficientFundsError) as context:
             self.a.transact(-500)
-        self.assertEqual(context.exception.args[0]['balance'], -500)
+        self.assertEqual(-500, context.exception.args[0]['balance'])
 
         self.b.transact(4000)
         with self.assertRaises(InsufficientFundsError) as context:
             self.b.transact(-10000)
-        self.assertEqual(context.exception.args[0]['balance'], -5000)
+        self.assertEqual(-5000, context.exception.args[0]['balance'])
--- OverdraftAccount.py
+++ OverdraftAccount.py
@@ -13,8 +13,8 @@ class OverdraftAccount(Account):
         return self.overdraft_account.balance
 
     def transact(self, amount: float) -> float:
-# withdraw -> take from balance first, then O/D
-# deposit -> put toward O/D first then balance
+        # withdraw -> take from balance first, then O/D
+        # deposit -> put toward O/D first then balance
         # withdrawal logic:
         if amount < 0:
             self.balance += amount
@@ -25,6 +25,7 @@ class OverdraftAccount(Account):
 
         return self.balance
 
+
 class TestOverdraftAccount(TestCase):
     def setUp(self) -> None:
         self.a = OverdraftAccount('Alice', 1, 500)
@@ -32,29 +33,29 @@ class TestOverdraftAccount(TestCase):
         self.c = OverdraftAccount('Carol', 3, 0)
 
     def test_overdraft_amounts(self):
-        self.assertEqual(self.a.get_overdraft_amount(), 500)
-        self.assertEqual(self.b.get_overdraft_amount(), 750)
-        self.assertEqual(self.c.get_overdraft_amount(), 0)
+        self.assertEqual(500, self.a.get_overdraft_amount())
+        self.assertEqual(750, self.b.get_overdraft_amount())
+        self.assertEqual(0, self.c.get_overdraft_amount())
 
     def test_overdraft_maximums(self):
-        self.assertEqual(self.a.overdraft_maximum, 500)
-        self.assertEqual(self.b.overdraft_maximum, 750)
-        self.assertEqual(self.c.overdraft_maximum, 0)
+        self.assertEqual(500, self.a.overdraft_maximum)
+        self.assertEqual(750, self.b.overdraft_maximum)
+        self.assertEqual(0, self.c.overdraft_maximum)
 
     def test_withdraw_with_overdraft(self):
         self.a.transact(-300)
-        self.assertEqual(self.a.balance, -300)
-        self.assertEqual(self.a.overdraft_account.balance, 200)
+        self.assertEqual(-300, self.a.balance)
+        self.assertEqual(200, self.a.overdraft_account.balance)
 
     def test_withdrawal_while_overdrawn(self):
         # withdraw 100 from an account with 0 and 500 o/d
         # balance should be -100 and od available 400
         self.a.transact(-100)
-        self.assertEqual(self.a.balance, -100)
-        self.assertEqual(self.a.overdraft_account.balance, 400)
+        self.assertEqual(-100, self.a.balance)
+        self.assertEqual(400, self.a.overdraft_account.balance)
 
         # withdraw 200 from an account already overdrawn (-100)
         # balance should be -300, and od available 200
         self.a.transact(-200)
-        self.assertEqual(self.a.balance, -300)
-        self.assertEqual(self.a.overdraft_account.balance, 200)
+        self.assertEqual(-300, self.a.balance)
+        self.assertEqual(200, self.a.overdraft_account.balance)
