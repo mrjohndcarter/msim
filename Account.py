7f3e6f9d8d1b4d982a530c9f0eae936068ed6b0a
John Carter <mrjohndcarter@gmail.com>
2020-04-09 12:24:07 -0400

    Added basic withdrawal

--- Account.py
+++ Account.py
@@ -13,6 +13,8 @@ class Account(object):
 class TestGetJiraDate(TestCase):
     def setUp(self) -> None:
         self.a = Account('Alice', 1)
+        self.b = Account('Bob', 2)
+        self.b.balance = 1000.0
 
     def test_new_acount(self):
         self.assertEqual(self.a.balance, 0)
@@ -22,3 +24,7 @@ class TestGetJiraDate(TestCase):
         self.assertEqual(self.a.balance, 505.50)
         self.assertEqual(self.a.transact(10), 515.50)
 
+    def test_withdrawal_no_overdraft(self):
+        self.b.transact(-500)
+        self.assertEqual(self.b.balance, 500)
+        self.assertEqual(self.b.transact(-100), 400)
